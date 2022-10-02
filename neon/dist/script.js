0a76bfe801fbebc7f1412711d6f0846181947a68
prabhuu5 <114859712+prabhuu5@users.noreply.github.com>
2022-10-02 20:19:22 +0530

    Add files via upload

---
+++ neon/dist/script.js
@@ -0,0 +1,60 @@
+var textHolder = document.getElementsByTagName('div')[0],
+  text = textHolder.innerHTML,
+	chars = text.length,
+	newText = '',
+	i;	
+
+for (i = 0; i < chars; i += 1) {
+	newText += '<i>' + text.charAt(i) + '</i>';
+}
+
+textHolder.innerHTML = newText;
+
+var letters = document.getElementsByTagName('i'),
+	flickers = [5, 7, 9, 11, 13, 15, 17],
+	randomLetter,
+	flickerNumber,
+	counter;
+
+function randomFromInterval(from,to) {
+	return Math.floor(Math.random()*(to-from+1)+from);
+}
+
+function hasClass(element, cls) {
+    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
+}
+
+function flicker() {		
+	counter += 1;
+	
+	if (counter === flickerNumber) {
+		return;
+	}
+
+	setTimeout(function () {
+		if(hasClass(randomLetter, 'off')) {
+			randomLetter.className = '';
+		}
+		else {
+			randomLetter.className = 'off';
+		}
+
+		flicker();
+	}, 30);
+}
+
+(function loop() {
+    var rand = randomFromInterval(500,3000);
+
+	randomLetter = randomFromInterval(0, 3);
+	randomLetter = letters[randomLetter];
+	
+	flickerNumber = randomFromInterval(0, 6);
+	flickerNumber = flickers[flickerNumber];
+
+    setTimeout(function() {
+            counter = 0;
+            flicker();
+            loop();  
+    }, rand);
+}());
